bbcbfe0ee419ff4c7f940978923d97c44110ecfe
LeticiaRocha20 <115549669+LeticiaRocha20@users.noreply.github.com>
2022-12-09 09:59:38 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let name = "Letícia" ;
+console.log(name) 
+
+let age = 19;
+console.log(age)
+
+console.log(7 * age)
+
+let friends = ["Débora", "Nana", "Luana"];
+let numbers = [1, 2, 3];
+console.log(friends[1]);
+
+let person = {
+    name: "Letícia",
+    age: 19,
+    isStudent: true
+  };
+  console.log(person.name);
+
+let students = [
+    {
+      name: "Débora",
+      age: 18,
+      isStudent: true
+    },
+    {
+      name: "Nana",
+      age: 24,
+      isStudent: true
+    },
+    {
+      name: "Luana",
+      age: 18,
+      isStudent: false
+    }
+  ];
+  console.log(students[0].name);
+
